bb6defa00260e3d993a260ffa8628aa10be32fc9
erenat77 <erenatmaca89@gmail.com>
2019-12-28 20:23:10 -0800

    added clarification

--- main.py
+++ main.py
@@ -102,9 +102,9 @@ if __name__ == "__main__":
         model = word2vec.Word2Vec.load("300features_40minwords_10context.emb")
 
     # Few tests: This will print the odd word among them 
-    print(model.wv.doesnt_match("man woman king queen princess dog".split()))
-    print(model.wv.doesnt_match("europe africa USA turkey".split()))
-    print(model.wv.most_similar("best"))
-    print(model.wv.most_similar("boring"))
+    print(model.wv.doesnt_match("man woman king queen princess".split()))
+    print("which one does not match at the following words ? \n europe africa USA turkey \n {}".format(model.wv.doesnt_match("europe africa USA turkey".split())))
+    print("Similar to 'best' :{} \n".format(model.wv.most_similar("best")))
+    print("Similar to 'best' : {} \n ".format(model.wv.most_similar("boring")))
     print(model.wv.most_similar_cosmul(positive=['man', 'woman'],\
                                     negative=['princess']))
